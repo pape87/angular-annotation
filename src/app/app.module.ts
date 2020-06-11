b932638e4e34cc33051024843ca31ba3fec68e19
Luca Perotti <lperotti@ranorex.com>
2020-06-11 15:21:43 +0200

    feat(app): add entity-selector component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EntitySelectorComponent } from './entity-selector/entity-selector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntitySelectorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/entity-selector/entity-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-entity-selector',
+  templateUrl: './entity-selector.component.html',
+  styleUrls: ['./entity-selector.component.scss']
+})
+export class EntitySelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
